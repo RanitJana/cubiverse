d156745c00d1bbcd243120d6ecfc9d934d885dde
Ranit Jana <ranitjana100@gmail.com>
2024-08-05 11:19:37 +0530

    update vercel

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,16 @@ import app from "./app.js";
 
 connectDB()
     .then(() => {
-        handleServer(app);
+        // handleServer(app);
+        const port = process.env.PORT || 5000;
+        app
+            .listen(port, () => {
+                console.log(`⚙ Server started at port : ${port}`);
+            })
+            .on('error', (err) => {
+                console.log(`An error occurred in express server : ${err}`);
+                throw new Error(err)
+            })
     })
     .catch((error) => {
         console.log(`Database connection error : ${error}`);
